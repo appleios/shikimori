2243169254bec19237d998fb4972bf1030096661
appleios <vm06lau@yandex-team.ru>
2018-04-26 11:09:55 +0300

    initial commit

---
+++ Shikimori/App/UI/Main/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  Shikimori
+//
+//  Created by Aziz Latipov on 25.04.2018.
+//  Copyright © 2018 shikimori.org. All rights reserved.
+//
+
+import UIKit
+
+
+class MainViewController: UIViewController {
+
+}
---
+++ Shikimori/Config/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Shikimori
+//
+//  Created by Aziz Latipov on 25.04.2018.
+//  Copyright © 2018 shikimori.org. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+    }
+
+}
